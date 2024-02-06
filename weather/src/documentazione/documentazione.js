2dd077a8428010b8afd3d907a0961e29c36fe004
musti <mustafa.vugdalic@marconirovereto.it>
2024-02-06 22:50:33 +0100

    Fine

--- weather/src/documentazione/documentazione.js
+++ weather/src/documentazione/documentazione.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Image1 from './pic1.gif';
+import Image2 from './pic2.gif';
 
 function DocumentationPage() {
+  const openUrl = () => {
+    // Replace 'your-url-here' with the actual URL you want to open
+    window.open('https://www.canva.com/design/DAF8DIZuP9E/TRoUNyPgzSShZHsOPj_T7A/view?utm_content=DAF8DIZuP9E&utm_campaign=designshare&utm_medium=link&utm_source=editor', '_blank');
+  };
+
   return (
     <div className="app documentazione-page">
       <header className="header">
@@ -16,10 +23,20 @@ function DocumentationPage() {
           </ul>
         </nav>
       </header>
-
       <div className="content">
-        <h2>Welcome to the Documentation Page!</h2>
-        <p>This is your documentation-related content.</p>
+        <div className="layer-container">
+          <div className="layer layer-left">
+            <img src={Image1} alt="1" width="300" height="300"/>
+            <p className="text-center">- Mustafa Vugdalic -</p>
+          </div>
+          <div className="layer layer-right">
+            <img src={Image2} alt="1" width="300" height="300"/>
+            <p className="text-center">- Serhii kushnir -</p>
+          </div>
+        </div>
+        <div className="layer button-layer">
+          <button onClick={openUrl}>Canva</button>
+        </div>
       </div>
     </div>
   );
